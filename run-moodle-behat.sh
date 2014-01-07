95789bb777f6725e634b243053fb600db376e75d
David Monllao <davidm@moodle.com>
2014-01-07 15:31:29 +0800

    Re-run only the failed scenario

--- run-moodle-behat.sh
+++ run-moodle-behat.sh
@@ -22,6 +22,7 @@ set -e
 behatcommand='vendor/bin/behat'
 
 # Upgrade behat site and update composer dependencies if necessary.
+echo "UPGRADING TEST SITE..."
 php admin/tool/behat/cli/init.php > /dev/null
 
 # Only to get the command, separated from the former one as we want proper output.
@@ -37,14 +38,29 @@ fi
 # Run behat stopping on failures, we want to detect the first one and run again against the next revision.
 # We set $failed because we need to continue in who-broke-it.sh until we find the issue.
 behatfullcommand="$behatrunner --stop-on-failure"
-exitcode=0
-echo $behatfullcommand;
-if ${behatrunner}; then
-    exitcode=$?
+
+# If there is already a failed scenario we only run that one.
+if [ "$failedscenario" != "" ]; then
+    behatfullcommand="$behatfullcommand $failedscenario"
+fi
+
+echo "RUNNING $behatfullcommand"
+if ! behatoutput=$( ${behatfullcommand} ); then
+
     failed=1
+
+    # Get the name of the failed scenario.
+    # We clean from the # where the file name begins until the : which
+    # informs about the line number.
+    line=$( echo "$behatoutput" | grep "From scenario " )
+    failedscenario=${line#*# }
+    export failedscenario=${failedscenario%:*}
+    echo "FAILED SCENARIO: $failedscenario"
 fi
 
 # Exit returning behat's error code as bisect run needs it.
 if [ "$1" == "1" ]; then
-    exit $exitcode
+    # Returning generic exit code as git bisect only accepts codes
+    # between 1 and 127 (excluding 125) so we need to control script's return.
+    exit 1
 fi
