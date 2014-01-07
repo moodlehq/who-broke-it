43271f57753e530240f5fe9e6f1766398bceb330
David Monllao <davidm@moodle.com>
2014-01-07 15:31:41 +0800

    Re-run only the failed test

--- run-moodle-phpunit.sh
+++ run-moodle-phpunit.sh
@@ -17,7 +17,7 @@
 set -e
 
 # Hardcoded vars.
-phpunitcommand='vendor/bin/phpunit --stop-on-failure'
+phpunitcommand='vendor/bin/phpunit'
 
 # Get composer.phar and install dependencies if they are not there.
 if [ -f "composer.phar" ]; then
@@ -28,18 +28,33 @@ else
 fi
 
 # Upgrade phpunit site.
+echo "UPGRADING TEST SITE..."
 php admin/tool/phpunit/cli/init.php > /dev/null
 
 # Run phpunit stopping on failures, we want to detect the first one and run again against the next revision.
 # We set $failed because we need to continue in who-broke-it.sh until we find the issue.
-echo $phpunitcommand
-exitcode=0
-if ${phpunitcommand}; then
-    exitcode=$?
+phpunitfullcommand="$phpunitcommand --stop-on-failure"
+
+# If there is already a failed test we only run that one.
+if [ "$failedtest" != "" ]; then
+    phpunitfullcommand="$phpunitfullcommand $failedtest"
+fi
+
+echo "RUNNING $phpunitfullcommand"
+if ! phpunitoutput=$( ${phpunitfullcommand} ); then
+
     failed=1
+
+    # Get the proposed re-run test which contains the failed file
+    line=$( echo "$phpunitoutput" | grep "vendor/bin/phpunit" | sed 's/^ *//g' )
+    export failedtest=$( echo "$line" | awk '{print $2,$3}' )
+    echo "FAILED TEST: $failedtest"
+
 fi
 
 # Exit returning behat's error code as bisect run needs it.
 if [ "$1" == "1" ]; then
-    exit $exitcode
+    # Returning generic exit code as git bisect only accepts codes
+    # between 1 and 127 (excluding 125) so we need to control script's return.
+    exit 1
 fi
